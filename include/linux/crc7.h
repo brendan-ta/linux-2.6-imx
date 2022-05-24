c9e9c92eb348650b922fb9dea2866a0acf3ca7b2
brendanta <brendan.ta@jacques.com.au>
2022-05-24 17:12:44 +1000

    Extend CRC7 checksum to support alternative lookup table
    The CRC7 checksum support has been extended to support a different
    lookup table (U-Boot CRC7 lookup table) which is used by the new
    crc7 calculation function (difference being this performs a left
    left).
    
    This however still adheres to the same CRC7 polynomial:
    x^7 + x^3 + 1 (binary: 10001000).

--- include/linux/crc7.h
+++ include/linux/crc7.h
@@ -3,12 +3,23 @@
 #include <linux/types.h>
 
 extern const u8 crc7_be_syndrome_table[256];
+extern const u8 crc7_uboot_syndrome_table[256];
+
+/*
+ * CRC7 Checksum with a left shift to match U-Boot's CRC7
+ * implementation
+ */
+static inline u8 crc7_uboot_byte(u8 crc, u8 data)
+{
+	return crc7_uboot_syndrome_table[(crc << 1) ^ data];
+}
 
 static inline u8 crc7_be_byte(u8 crc, u8 data)
 {
 	return crc7_be_syndrome_table[crc ^ data];
 }
 
+extern u8 crc7_uboot(u8 crc, const u8 *buffer, size_t len);
 extern u8 crc7_be(u8 crc, const u8 *buffer, size_t len);
 
 #endif
